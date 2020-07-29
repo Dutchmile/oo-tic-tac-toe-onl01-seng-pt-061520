26fc4ec2c1d3a9786702a72b8e2fe4e4bc8c5849
Christopher Dutcher <cadutcher@icloud.com>
2020-07-29 19:33:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -82,10 +82,9 @@ def display_board
     WIN_COMBINATIONS.each do |array|
       if @board[array[0]] == @board[array[1]] && @board[array[0]] == @board[array[2]] && position_taken?(array[0])
         true
-      else
-        false
       end
     end
+    false
   end
 
 
